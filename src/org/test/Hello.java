3f62c50f8bc806f3ea33b9f0cf4ad4fd4cbf056d
Vijicaloo <vijicaloo@gmail.com>
2021-11-22 14:58:57 +0530

    Hello

---
+++ src/org/test/Hello.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class Hello {
+	public static void main(String[] args) {
+		int age=15;
+		if(age<18){
+		
+		System.out.println("Not eligible  to vote" );
+		}
+		else{
+			System.out.println("Eligible to vote");
+		}
+		}
+
+}
