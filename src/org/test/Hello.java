55787ad90b9d6523b2efa26b43edcfbc64a25b4f
Vijicaloo <vijicaloo@gmail.com>
2021-11-22 21:43:19 +0530

    rajigitfirst

--- src/org/test/Hello.java
+++ src/org/test/Hello.java
@@ -2,15 +2,18 @@ package org.test;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Eligibility");
-		int age=58;
+		System.out.println("eligibility");
+		int age=66;
 		if(age<18){
-		
 		System.out.println("Not eligible  to vote" );
 		}
-		else{
+		else if(age>=18 && age<60){
 			System.out.println("Eligible to vote");
 		}
+		
+		else if(age>=60) {
+			System.out.println("Eligible to vote - Senior citizen");
 		}
-
+		
+		}
 }
